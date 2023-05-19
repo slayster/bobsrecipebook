179d791e6c688e4e298a41d92e2550f26148566f
Brendan Ellis <brendan@slayweb.com>
2023-05-19 21:23:11 +0000

    recipes migration

---
+++ database/migrations/2023_05_19_193458_create_recipe_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRecipeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recipes', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user');
+            $table->string('title');
+            $table->longText('method');
+            $table->integer('servings');
+            $table->integer('carbs');
+            $table->integer('calories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recipes');
+    }
+}
